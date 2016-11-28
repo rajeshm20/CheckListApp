22ddaf3a5f5682bff2561eb6d7d6a8267fc93a17
Rajesh M <rajeshm@rajeshs-mac.local>
2016-11-28 22:53:44 +0530

    Initial Commit

---
+++ TableViewCheckList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewCheckList
+//
+//  Created by Rajesh M on 28/11/16.
+//  Copyright © 2016 Rajesh M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewCheckList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableViewCheckList
+//
+//  Created by Rajesh M on 28/11/16.
+//  Copyright © 2016 Rajesh M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
